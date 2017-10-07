ce6ed0ce6d9e1f0e7b4dd06c33e7a8f1a50d0aa1
Jiankian <luozhengan@vip.qq.com>
2017-10-07 12:10:36 +0800

    测试成功

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -2,16 +2,48 @@
 namespace app\index\controller;
 
 use think\Controller;
+use think\facade\Request;
+use \Firebase\JWT\JWT;
 
 class Index extends Controller
 {
+    private  $key = "@#$^^^&*&*(&*&GHJHks#$56bHULKJ^^*&**";
     public function index()
     {
         return $this->fetch();
     }
 
-    public function hello($name = 'ThinkPHP5')
-    {
-        return 'hello,' . $name;
+    public function login(){
+        return $this->fetch();
+    }
+
+    public function signin(){
+        $username = Request::post('username');
+        $password = Request::post('password');
+        $this->assign('username',$username);
+        $this->assign('password',$password);
+
+        $token = array(
+            "user" => $username,
+            "pwd" => $password,
+            "iat" => 1356999524,
+            "nbf" => 1357000000
+        );
+
+        $jwt = JWT::encode($token, $this->key);
+        $this->assign('token',$jwt);
+        return $this->fetch();
+    }
+
+    public function detoken(){
+        return $this->fetch();
+    }
+
+    public function token(){
+        $tokens = Request::post('token');
+        $decoded = JWT::decode($tokens, $this->key, array('HS256'));
+//        print_r($decoded);
+        $this->assign('token',$decoded);
+        return $this->fetch();
     }
 }
